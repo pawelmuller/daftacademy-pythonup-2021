202c4ec1ed12f6c845be516f304ecf2bda4c2c2b
Paweł Muller <pawelmuller42@gmail.com>
2021-04-14 21:33:47 +0200

    Added auth endpoint tests.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from hashlib import sha512
 
 client = TestClient(app)
 
@@ -7,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == {"message": "Hello world!"}
 
 
 def test_hello_name():
@@ -47,3 +48,22 @@ def test_method_return():
     response = client.post(f"/method")
     assert response.status_code == 201
     assert response.json() == {"method": "POST"}
+
+
+def test_auth():
+    passwords = ['pass', 778866123, 'pear', 'bearDrunkBeer']
+    for password in passwords:
+        password_hash = sha512(str(password).encode('UTF-8')).hexdigest()
+        response = client.get(f"/auth?password={password}&password_hash={password_hash}")
+        assert response.status_code == 204
+
+    for password in passwords:
+        password_hash = 'wrongHashActually'
+        response = client.get(f"/auth?password={password}&password_hash={password_hash}")
+        assert response.status_code == 401
+
+    response = client.get(f"/auth?password=&password_hash='something'")
+    assert response.status_code == 401
+
+    response = client.get(f"/auth?password='something&password_hash=")
+    assert response.status_code == 401
